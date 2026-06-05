7f9b75ef259e4fe09e6538c47f8e7aaea2498ad2
Roee Ovadia <roee@sahbak.co.il>
2026-06-05 03:19:16 +0300

    updaet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	i18n: {
-		defaultLocale: 'he',
-		locales: ['he', 'en'],
-		routing: {
-			prefixDefaultLocale: false,
-		},
-	},
+  site: "https://gosites-co-il.github.io",
+  base: "/visitvineo",
+
+  i18n: {
+    defaultLocale: "he",
+    locales: ["he", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
